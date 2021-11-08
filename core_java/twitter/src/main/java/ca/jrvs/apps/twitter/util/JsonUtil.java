189a1679c4e168fc4fc43ccc27a73e75d769f853
Abdul-Samad Sethi <abdulsamadsethi@gmail.com>
2021-11-08 23:23:56 +0000

    Fixed formatting problem within TweetUtil and JsonUtil

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/util/JsonUtil.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/util/JsonUtil.java
@@ -2,19 +2,19 @@ package ca.jrvs.apps.twitter.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import java.io.IOException;
 
 public class JsonUtil {
 
-    /**
-     * Convert a java object to a JSON string
-     *
-     * @param object - Input object
-     * @return JSON string
-     * @throws JsonProcessingException
-     */
+    public static <T> T toObjectFromJson(String json, Class cls) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return (T) objectMapper.readValue(json, cls);
+    }
+
     public static String toJson(Object object, boolean prettyJson, boolean includeNullValues)
             throws JsonProcessingException {
         ObjectMapper m = new ObjectMapper();
@@ -25,19 +25,5 @@ public class JsonUtil {
             m.enable(SerializationFeature.INDENT_OUTPUT);
         }
         return m.writeValueAsString(object);
-    }
-
-    /**
-     * Parse JSON string to an object
-     *
-     * @param json  - JSON string
-     * @param clazz - Object class
-     * @param <T>   - Type
-     * @return Object
-     * @throws IOException
-     */
-    public static <T> T toObjectFromJson(String json, Class clazz) throws IOException {
-        ObjectMapper m = new ObjectMapper();
-        return (T) m.readValue(json, clazz);
     }
 }
