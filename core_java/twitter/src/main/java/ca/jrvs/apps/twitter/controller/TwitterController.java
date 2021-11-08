d323522a7752e02efa54c881b48ac6e77ae1e1ce
Abdul-Samad Sethi <abdulsamadsethi@gmail.com>
2021-11-08 23:16:58 +0000

    Fixing syntax errors in TwitterController file

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/TwitterController.java
@@ -1,24 +1,22 @@
 package ca.jrvs.apps.twitter.controller;
 
-import ca.jrvs.apps.twitter.model.Coordinates;
 import ca.jrvs.apps.twitter.model.Tweet;
 import ca.jrvs.apps.twitter.service.Service;
 import ca.jrvs.apps.twitter.util.TweetUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @org.springframework.stereotype.Controller
-public class TwitterController implements Controller {
+public class TwitterController implements Controller{
 
     private static final String COORD_SEP = ":";
     private static final String COMMA = ",";
-
     private Service service;
 
     @Autowired
-    public TwitterController(Service service) { this.service = service; }
+    public TwitterController(Service service){
+        this.service = service;
+    }
 
     /**
      * Parse user argument and post a tweet by calling service classes
@@ -28,26 +26,30 @@ public class TwitterController implements Controller {
      * @throws IllegalArgumentException if args are invalid
      */
     @Override
-    public Tweet postTweet(String[] args) throws IllegalArgumentException {
-        if (args.length < 3) {
-            throw new IllegalArgumentException("Usage: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\"");
+    public Tweet postTweet(String[] args) {
+        if (args.length != 3){
+            throw new IllegalArgumentException("Invalid Number of Arguments");
         }
-        Tweet tweet =  new Tweet();
+
         String text = args[1];
         String coords = args[2];
-        String[] coordArr = coords.split(COORD_SEP);
-        if (coordArr.length < 2) {
-            throw new IllegalArgumentException("Invalid cooridnates. Usage: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\"");
+        String[] corr = coords.split(COORD_SEP);
+
+        if (corr.length < 2){
+            throw new IllegalArgumentException("Invalid Number of Coordinates for tweet");
         }
+
+        Double lng;
         Double lat;
-        Double lon;
-        try {
-            lat = Double.parseDouble(coordArr[0]);
-            lon = Double.parseDouble(coordArr[1]);
-        } catch (Exception ex){
-            throw new IllegalArgumentException("Invalid cooridnates. Usage: TwitterCLIApp post \"tweet_text\" \"latitude:longitude\"");
+
+        try{
+            lng = Double.parseDouble(corr[0]);
+            lat = Double.parseDouble(corr[1]);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("Invalid Input: Error parsing coordinates", e);
         }
-        tweet = TweetUtil.buildTweet(text, lat, lon);
+
+        Tweet tweet = TweetUtil.createTweet(text, lng, lat);
         return service.postTweet(tweet);
     }
 
@@ -60,17 +62,12 @@ public class TwitterController implements Controller {
      */
     @Override
     public Tweet showTweet(String[] args) {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Usage: TwitterCLIApp show tweet_id [field1,fields2]");
+        if (args.length < 2){
+            throw new IllegalArgumentException("Invalid Number of Arguments");
         }
-        Tweet tweet =  new Tweet();
-        String id = args[1];
-        String[] fieldsArr = {};
-        if (args.length == 3) {
-            String fields = args[2];
-            fieldsArr = fields.split(COMMA);
-        }
-        return service.showTweet(id, fieldsArr);
+
+        String[] fields = args.length > 2 ? args[2].split(COMMA) : null;
+        return service.showTweet(args[1], fields);
     }
 
     /**
@@ -82,10 +79,11 @@ public class TwitterController implements Controller {
      */
     @Override
     public List<Tweet> deleteTweet(String[] args) {
-        if (args.length < 2) {
-            throw new IllegalArgumentException("Usage: TwitterApp delete [id1,id2,..]");
+        if (args.length < 2){
+            throw new IllegalArgumentException("Invalid Number of Arguments");
         }
-        String[] deleteIds = args[1].split(COMMA);
-        return service.deleteTweets(deleteIds);
+
+        String[] ids = args[1].split(COMMA);
+        return service.deleteTweets(ids);
     }
 }
